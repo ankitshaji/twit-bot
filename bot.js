1de2765cb3b1b880f0f02578a1fda8a1ec099881
Ankit Shaji <ankitshaji@gmail.com>
2022-06-27 15:50:54 +1200

    GET request-search/tweets-show text values of tweets

--- bot.js
+++ bot.js
@@ -5,3 +5,17 @@ const config = require("./config");
 
 //Creating insatance of Twit()
 const T = new Twit(config);
+
+//GET request - search/tweets - query(skate,2) 
+T.get(
+  "search/tweets",
+  { q: "skate", count: 2 },
+  function (err, data, response) {
+    //output just the text of each tweet
+    const tweets = data.statuses;
+    for (let i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
+      console.log("break");
+    }
+  }
+);
