78cef209b1e76e7007722beda523182e89740ad4
Ankit Shaji <ankitshaji@gmail.com>
2022-06-27 21:58:24 +1200

    POST request upload image file + status

--- bot.js
+++ bot.js
@@ -2,11 +2,13 @@ console.log("The bot is running");
 //import Twit and auth info
 const Twit = require("twit");
 const config = require("./config");
+const fs = require("fs");
+
 //Creating insatance of Twit()
 const T = new Twit(config);
 
-// searchTweets();
 //GET request - search/tweets - query(skate,2)
+// searchTweets();
 function searchTweets() {
   T.get(
     "search/tweets",
@@ -21,12 +23,43 @@ function searchTweets() {
     }
   );
 }
+//Post Request - uplod media to twitter to get ID
+function uploadMedia() {
+  //import file from local
+  const fileName = "./images/dirt.jpg";
+  const b64 = fs.readFileSync(fileName, { encoding: "base64" });
+  //POST Request
+  T.post("media/upload", { media_data: b64 }, postTweet);
+}
 
-// postTweet();
-// setInterval(postTweet,100*30);
-//POST request - post tweet
-function postTweet() {
-  //Different status check
+//POST request - Post tweet with image
+function postTweet(err, data, response) {
+  console.log(data);
+  const id = data.media_id_string;
+  console.log(id)
+  //random no generator
+  const r = Math.floor(Math.random() * 100);
+  T.post(
+    "statuses/update",
+    {
+      status: "Test Tweet, random no: " + r + " : Post Request",
+      media_ids: [id],
+    },
+    function (err, data, response) {
+      if (err) {
+        console.log("Error occured when posting.");
+      } else {
+        console.log("Tweet Posted successfully.");
+      }
+    }
+  );
+}
+uploadMedia();
+// setInterval(uploadMedia,1000 * 60 * 60);
+
+//POST request - Post new status tweet
+function postStatusTweet() {
+  //Random no
   const r = Math.floor(Math.random() * 1000*60*60);
   T.post(
     "statuses/update",
@@ -40,3 +73,4 @@ function postTweet() {
     }
   );
 }
+//postStatusTweet();
