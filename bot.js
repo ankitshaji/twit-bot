2ad2d08bb3886cb2bb87495bca87c6561b583276
Ankit Shaji <ankitshaji@gmail.com>
2022-06-27 14:57:53 +1200

    creating twit instance - getting auth info

--- bot.js
+++ bot.js
@@ -1,1 +1,7 @@
 console.log("The bot is running");
+//import Twit and auth info
+const Twit = require("twit");
+const config = require("./config");
+
+//Creating insatance of Twit()
+const T = new Twit(config);
