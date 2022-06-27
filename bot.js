7b17c2109adeaf9070c499cb2a197fea1f9973c9
Ankit Shaji <ankitshaji@gmail.com>
2022-06-27 20:08:51 +1200

    POST request - post a tweet with random statusno

--- bot.js
+++ bot.js
@@ -2,20 +2,40 @@ console.log("The bot is running");
 //import Twit and auth info
 const Twit = require("twit");
 const config = require("./config");
-
 //Creating insatance of Twit()
 const T = new Twit(config);
 
-//GET request - search/tweets - query(skate,2) 
-T.get(
-  "search/tweets",
-  { q: "skate", count: 2 },
-  function (err, data, response) {
-    //output just the text of each tweet
-    const tweets = data.statuses;
-    for (let i = 0; i < tweets.length; i++) {
-      console.log(tweets[i].text);
-      console.log("break");
+// searchTweets();
+//GET request - search/tweets - query(skate,2)
+function searchTweets() {
+  T.get(
+    "search/tweets",
+    { q: "skate", count: 2 },
+    function (err, data, response) {
+      //output just the text of each tweet
+      const tweets = data.statuses;
+      for (let i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+        console.log("break");
+      }
     }
-  }
-);
+  );
+}
+
+// postTweet();
+//POST request - post tweet
+function postTweet() {
+  //Different status check
+  const r = Math.floor(Math.random() * 100);
+  T.post(
+    "statuses/update",
+    { status: "Test Tweet, random no: " + r + " : Post Request" },
+    function (err, data, response) {
+      if (err) {
+        console.log("Error occured when posting.");
+      } else {
+        console.log("Tweet Posted successfully.");
+      }
+    }
+  );
+}
